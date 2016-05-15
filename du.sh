a312d05a22b13decc0394b5339e649e69e0a4393
Nathan Chancellor <natechancellor@gmail.com>
2016-05-15 14:53:32 -0700

    Add ROM build scripts
    Thank you to @ezio84 for the ideas behind this. These will allow you to
    easily compile Dirty Unicorns and Pure Nexus with only a few edits. One
    command to rule them all :P

---
+++ du.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# DIRTY UNICORNS BUILD SCRIPT
+# Build Dirty Unicorns with one easy script. You will need to have the repo synced and configured already, use this page to help you with that: https://raw.githubusercontent.com/nathanchance/Android-Tools/master/Building_AOSP.txt
+
+# Usage:
+# $ . du.sh <device> <sync|nosync> <clean|noclean>
+# Parameter 1: Device you want to build (angler, hammerhead, bullhead, etc.)
+# Parameter 2: Do you want to perform a repo sync or not?
+# Parameter 3: Do you want to make clobber or make installclean?
+
+# Examples:
+# . du.sh angler sync clean
+# . du.sh hammerhead nosync noclean
+
+# Necessary edits:
+# SOURCEDIR: The directory that holds your DU repos
+# HOLDDIR: The directory that will hold your completed DU zip files
+# KBUILD_BUILD_HOST section: Remove this section if you don't want a custom user@host in the kernel version
+# DU_BUILD_TYPE section: Remove this section if you want to stick with DIRTY-DEEDS as the DU version.
+
+# Colors
+BLDRED="\033[1m""\033[31m"
+RST="\033[0m"
+
+# Variables
+SOURCEDIR=???
+OUTDIR=${SOURCEDIR}/out/target/product
+HOLDDIR=???
+
+# Parameters
+DEVICE=$1
+SYNC=$2
+CLEAN=$3
+
+# Make it show custom user@host in the kernel version
+export KBUILD_BUILD_USER=???
+export KBUILD_BUILD_HOST=???
+
+# Make a custom DU build tag
+export DU_BUILD_TYPE=???
+
+# Start tracking time
+echo -e ""
+echo -e ${BLDRED}"SCRIPT STARTING AT $(date +%D\ %r)"${RST}
+echo -e ""
+START=$(date +%s)
+
+# Change to the source directory
+echo -e ${BLDRED}"MOVING TO ${SOURCEDIR}"${RST}
+echo -e ""
+cd ${SOURCEDIR}
+
+# Sync the repo if requested
+if [ "${SYNC}" == "sync" ]
+then
+   echo -e ${BLDRED}"SYNCING LATEST SOURCES"${RST}
+   echo -e ""
+   repo sync
+fi
+
+# Setup the build environment
+echo -e ${BLDRED}"SETTING UP BUILD ENVIRONMENT"${RST}
+echo -e ""
+. build/envsetup.sh
+echo -e ""
+
+# Prepare device
+echo -e ${BLDRED}"PREPARING ${DEVICE}"${RST}
+echo -e ""
+breakfast ${DEVICE}
+
+# Clean up
+echo -e ${BLDRED}"CLEANING UP ${SOURCEDIR}/out"${RST}
+echo -e ""
+if [ "${CLEAN}" == "clean" ]
+then
+   make clobber
+else
+   make installclean
+fi
+
+# Start building
+echo -e ${BLDRED}"MAKING ZIP FILE"${RST}
+echo -e ""
+mka bacon
+echo -e ""
+
+# Remove exisiting files in HOLDDIR
+echo -e ${BLDRED}"REMOVING FILES IN ${HOLDDIR}"${RST}
+echo -e ""
+rm ${HOLDDIR}/*_${DEVICE}_*.zip
+rm ${HOLDDIR}/*_${DEVICE}_*.zip.md5sum
+
+# Copy new files to HOLDDIR
+echo -e ${BLDRED}"MOVING FILES FROM ${OUTDIR} TO ${HOLDDIR}"${RST}
+echo -e ""
+mv ${OUTDIR}/${DEVICE}/DU_${DEVICE}_*.zip ${HOLDDIR}
+mv ${OUTDIR}/${DEVICE}/DU_${DEVICE}_*.zip.md5sum ${HOLDDIR}
+
+# Go back home
+echo -e ${BLDRED}"GOING HOME"${RST}
+echo -e ""
+cd ~/
+
+# Stop tracking time
+echo -e ${BLDRED}"SCRIPT ENDING AT $(date +%D\ %r)"${RST}
+echo -e ""
+END=$(date +%s)
+
+# Successfully completed compilation
+echo -e ${BLDRED}"====================================="${RST}
+echo -e ${BLDRED}"Compilation and upload successful!"${RST}
+echo -e ${BLDRED}"Total time elapsed: $(echo $(($END-$START)) | awk '{print int($1/60)"mins "int($1%60)"secs"}')"${RST}
+echo -e ${BLDRED}"====================================="${RST}
---
+++ pn_cmte.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+# PURE NEXUS CMTE BUILD SCRIPT
+# Build Pure Nexus with one easy script. You will need to have the repo synced and configured already, use this page to help you with that: https://raw.githubusercontent.com/nathanchance/Android-Tools/master/Building_AOSP.txt
+
+# Usage:
+# $ . pn_cmte.sh <device> <sync|nosync> <clean|noclean>
+# Parameter 1: Device you want to build (angler, hammerhead, bullhead, etc.)
+# Parameter 2: Do you want to perform a repo sync or not?
+# Parameter 3: Do you want to make clobber or make installclean?
+
+# Examples:
+# . pn_cmte.sh angler sync clean
+# . pn_cmte.sh hammerhead nosync noclean
+
+# Necessary edits:
+# SOURCEDIR: The directory that holds your PN repos
+# HOLDDIR: The directory that will hold your completed PN zip files
+# KBUILD_BUILD_HOST section: Remove this section if you don't want a custom user@host in the kernel version
+
+# Colors
+BLDBLUE="\033[1m""\033[36m"
+RST="\033[0m"
+
+# Variables
+SOURCEDIR=???
+OUTDIR=${SOURCEDIR}/out/target/product
+HOLDDIR=???
+
+# Parameters
+DEVICE=$1
+SYNC=$2
+CLEAN=$3
+
+# Make it show custom user@host in the kernel version
+export KBUILD_BUILD_USER=???
+export KBUILD_BUILD_HOST=???
+
+# Start tracking time
+echo -e ""
+echo -e ${BLDBLUE}"SCRIPT STARTING AT $(date +%D\ %r)"${RST}
+echo -e ""
+START=$(date +%s)
+
+# Change to the source directory
+echo -e ${BLDBLUE}"MOVING TO ${SOURCEDIR}"${RST}
+echo -e ""
+cd ${SOURCEDIR}
+
+# Sync the repo if requested
+if [ "${SYNC}" == "sync" ]
+then
+   echo -e ${BLDBLUE}"SYNCING LATEST SOURCES"${RST}
+   echo -e ""
+   repo sync
+fi
+
+# Setup the build environment
+echo -e ${BLDBLUE}"SETTING UP BUILD ENVIRONMENT"${RST}
+echo -e ""
+. build/envsetup.sh
+echo -e ""
+
+# Prepare device
+echo -e ${BLDBLUE}"PREPARING ${DEVICE}"${RST}
+echo -e ""
+breakfast ${DEVICE}
+
+# Clean up
+echo -e ${BLDBLUE}"CLEANING UP ${SOURCEDIR}/out"${RST}
+echo -e ""
+if [ "${CLEAN}" == "clean" ]
+then
+   make clobber
+else
+   make installclean
+fi
+
+# Start building
+echo -e ${BLDBLUE}"MAKING ZIP FILE"${RST}
+echo -e ""
+mka bacon
+echo -e ""
+
+# Remove exisiting files in HOLDDIR
+echo -e ${BLDBLUE}"REMOVING FILES IN ${HOLDDIR}"${RST}
+echo -e ""
+rm ${HOLDDIR}/*_${DEVICE}_*.zip
+rm ${HOLDDIR}/*_${DEVICE}_*.zip.md5sum
+
+# Copy new files to HOLDDIR
+echo -e ${BLDBLUE}"MOVING FILES FROM ${OUTDIR} TO ${HOLDDIR}"${RST}
+echo -e ""
+mv ${OUTDIR}/${DEVICE}/pure_nexus_${DEVICE}-*.zip ${HOLDDIR}
+mv ${OUTDIR}/${DEVICE}/pure_nexus_${DEVICE}-*.zip.md5sum ${HOLDDIR}
+
+# Go back home
+echo -e ${BLDBLUE}"GOING HOME"${RST}
+echo -e ""
+cd ~/
+
+# Stop tracking time
+echo -e ${BLDBLUE}"SCRIPT ENDING AT $(date +%D\ %r)"${RST}
+echo -e ""
+END=$(date +%s)
+
+# Successfully completed compilation
+echo -e ${BLDBLUE}"====================================="${RST}
+echo -e ${BLDBLUE}"Compilation and upload successful!"${RST}
+echo -e ${BLDBLUE}"Total time elapsed: $(echo $(($END-$START)) | awk '{print int($1/60)"mins "int($1%60)"secs"}')"${RST}
+echo -e ${BLDBLUE}"====================================="${RST}
