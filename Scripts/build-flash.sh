7e748cf80e80d40f3f81b2d1d71bee2881f78cf7
Nathan Chancellor <natechancellor@gmail.com>
2016-10-04 15:35:13 -0700

    Scripts: change Ninja script to Flash script
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- Scripts/build-flash.sh
+++ Scripts/build-flash.sh
@@ -4,7 +4,7 @@
 ##################
 ##  HOW TO USE  ##
 ##################
-# Copy this script to a directory other than the source, edit the variables below, and run "source build-ninja.sh"
+# Copy this script to a directory other than the source, edit the variables below, and run either "source build-flash.sh" or "bash build-flash.sh"
 
 
 #################
@@ -12,16 +12,16 @@
 #################
 # These MUST be edited for the script to work
 
-# SOURCE_DIR: Directory that holds your Ninja source
-# e.g. SOURCE_DIR=${HOME}/Android/Ninja
+# SOURCE_DIR: Directory that holds your Flash source
+# e.g. SOURCE_DIR=${HOME}/Android/angler
 SOURCE_DIR=
 
 # TOOLCHAIN_DIR: Directory that holds the toolchain repo
 # e.g. TOOLCHAIN_DIR=${HOME}/Android/aarch64-linux-android-6.x-kernel-linaro
 TOOLCHAIN_DIR=
 
-# NINJA_BRANCH: The branch that you want to compile on
-NINJA_BRANCH=release
+# FLASH_BRANCH: The branch that you want to compile on
+FLASH_BRANCH=release
 
 # Check user input
 if [[ -z ${SOURCE_DIR} ]]; then
@@ -57,6 +57,7 @@ function newLine() {
    echo -e ""
 }
 
+
 #######################
 ##  OTHER VARIABLES  ##
 #######################
@@ -66,9 +67,10 @@ BLINK_RED="\033[05;31m"
 RESTORE="\033[0m"
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
 KERNEL="Image.gz-dtb"
-DEFCONFIG="ninja_defconfig"
+DEFCONFIG="flash_defconfig"
 ZIMAGE_DIR="${SOURCE_DIR}/arch/arm64/boot"
 ANYKERNEL_DIR=${SOURCE_DIR}/anykernel
+DEVICE=angler
 
 
 ####################
@@ -86,13 +88,13 @@ clear
 
 # Show the version of the kernel compiling
 echo -e ${RED}; newLine
-echo -e "====================================================================="; newLine; newLine
-echo -e "    _   _______   __    _____       __ __ __________  _   __________ ";
-echo -e "   / | / /  _/ | / /   / /   |     / //_// ____/ __ \/ | / / ____/ / ";
-echo -e "  /  |/ // //  |/ /_  / / /| |    / ,<  / __/ / /_/ /  |/ / __/ / /  ";
-echo -e " / /|  // // /|  / /_/ / ___ |   / /| |/ /___/ _, _/ /|  / /___/ /___";
-echo -e "/_/ |_/___/_/ |_/\____/_/  |_|  /_/ |_/_____/_/ |_/_/ |_/_____/_____/"; newLine; newLine
-echo -e "====================================================================="; newLine; newLine
+echo -e "======================================================================="; newLine; newLine
+echo -e "    ________    ___   _____ __  __    __ __ __________  _   __________ "
+echo -e "   / ____/ /   /   | / ___// / / /   / //_// ____/ __ \/ | / / ____/ / "
+echo -e "  / /_  / /   / /| | \__ \/ /_/ /   / ,<  / __/ / /_/ /  |/ / __/ / /  "
+echo -e " / __/ / /___/ ___ |___/ / __  /   / /| |/ /___/ _, _/ /|  / /___/ /___"
+echo -e "/_/   /_____/_/  |_/____/_/ /_/   /_/ |_/_____/_/ |_/_/ |_/_____/_____/"; newLine; newLine
+echo -e "======================================================================="; newLine; newLine
 
 
 # Start tracking time
@@ -110,8 +112,8 @@ rm -rf ${KERNEL} > /dev/null 2>&1
 
 # Clean source directory
 cd ${SOURCE_DIR}
-git checkout ${NINJA_BRANCH}
-git reset --hard origin/${NINJA_BRANCH}
+git checkout ${FLASH_BRANCH}
+git reset --hard origin/${FLASH_BRANCH}
 git clean -f -d -x > /dev/null 2>&1
 git pull
 
@@ -122,6 +124,8 @@ make mrproper
 
 # Set kernel version
 KERNEL_VER=$( grep -r "EXTRAVERSION = -" ${SOURCE_DIR}/Makefile | sed 's/EXTRAVERSION = -//' )
+# Set zip name based on device and kernel version
+ZIP_NAME=${KERNEL_VER}-${DEVICE}
 
 
 # Make the kernel
@@ -141,7 +145,7 @@ if [[ `ls ${ZIMAGE_DIR}/${KERNEL} 2>/dev/null | wc -l` != "0" ]]; then
 
    cp -vr ${ZIMAGE_DIR}/${KERNEL} ${ANYKERNEL_DIR}/zImage
    cd ${ANYKERNEL_DIR}
-   zip -r9 ${KERNEL_VER}.zip * -x README ${KERNEL_VER}.zip
+   zip -r9 ${ZIP_NAME}.zip * -x README ${ZIP_NAME}.zip
 
 else
    BUILD_RESULT_STRING="BUILD FAILED"
@@ -153,13 +157,14 @@ cd ${HOME}
 
 
 # End the script
-newLine; echoText "${BUILD_RESULT_STRING}"; newLine
+newLine; echoText "${BUILD_RESULT_STRING}!"; newLine
 
 DATE_END=$(date +"%s")
 DIFF=$((${DATE_END} - ${DATE_START}))
 
 echo -e ${RED}"TIME: $((${DIFF} / 60)) MINUTES AND $((${DIFF} % 60)) SECONDS"
 if [[ "${BUILD_RESULT_STRING}" == "BUILD SUCCESSFUL" ]]; then
-   echo -e "COMPLETED ZIP: ${ANYKERNEL_DIR}/${KERNEL_VER}.zip"
+   echo -e "ZIP LOCATION: ${ANYKERNEL_DIR}/${ZIP_NAME}.zip"
+   echo -e "SIZE: $( du -h ${ZIP_MOVE}/${ZIP_NAME}.zip | awk '{print $1}' )"
 fi
 echo -e ${RESTORE}
