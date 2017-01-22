98760b2cb1938a3faef409d0a4b85681dd7818f1
Nathan Chancellor <natechancellor@gmail.com>
2017-01-21 22:03:49 -0700

    Completely overhaul repo
    All:
    1. License everything; you can never have enough GPL :)
    2. Try to keep everything with 80 lines for easier reading on the web
    3. 3 spaces -> 4 spaces for indents/tabs
    
    Guides:
    1. Update AOSP guide for N a little bit more and add a link to Arch instructions
    2. Update Flash guide since I have done a lot of repo shuffling
    
    Scripts:
    1. Remove ROM specific scripts and unify into one; I have devised a way to pull the ROM files directly from the out folder without knowing anything about the ROM and its formatting for zips.
    2. Update Flash script to do everything for the user without any input other than one variable.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- Scripts/build-flash.sh
+++ Scripts/build-flash.sh
@@ -1,101 +1,122 @@
 #!/bin/bash
+#
+# Flash Kernel build script
+#
+# Copyright (C) 2016-2017 Nathan Chancellor
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 
 ##################
-##  HOW TO USE  ##
+#                #
+#   HOW TO USE   #
+#                #
 ##################
-# Copy this script to a directory other than the source, edit the variables below, and run either "source build-flash.sh" or "bash build-flash.sh"
+#
+# Copy this script to a directory other than the source,
+# edit the variables below, and run either of the two commands:
+# $ source build-flash.sh
+# $ bash build-flash.sh
 
 
-#################
-##  Variables  ##
-#################
-# These MUST be edited for the script to work
+###############
+#             #
+#  VARIABLES  #
+#             #
+###############
+#
+# This variable MUST be edited for the script to work
+#
+# KERNEL_HOME is the folder that will hold all of the kernel files; if it does
+# not exist, it will be created. After that, everything either either be cloned
+# or updated automatically before building
 
-# SOURCE_DIR: Directory that holds your Flash source
-# e.g. SOURCE_DIR=${HOME}/Android/angler
-SOURCE_DIR=
+KERNEL_HOME=
 
-# ANYKERNEL_DIR: Directory that holds your AnyKernel source
-# e.g. ANYKERNEL_DIR=${HOME}/Android/AnyKernel2
-ANYKERNEL_DIR=
-
-# TOOLCHAIN_DIR: Directory that holds the toolchain repo
-# e.g. TOOLCHAIN_DIR=${HOME}/Android/aarch64-linux-android-6.x-kernel-linaro
-TOOLCHAIN_DIR=
-
-# FLASH_BRANCH: The branch that you want to compile on
-FLASH_BRANCH=release
 
 # Check user input
-if [[ -z ${SOURCE_DIR} ]]; then
-   echo "You did not edit the SOURCE_DIR variable! Please edit that variable at the top of the script and run it again."
-   exit
-fi
-
-if [[ -z ${ANYKERNEL_DIR} ]]; then
-   echo "You did not edit the ANYKERNEL_DIR variable! Please edit that variable at the top of the script and run it again."
-   exit
-fi
-
-if [[ -z ${TOOLCHAIN_DIR} ]]; then
-   echo "You did not edit the TOOLCHAIN_DIR variable above! Please edit that variable at the top of the script and run it again."
-   exit
+if [[ -z ${KERNEL_HOME} ]]; then
+    echo "You did not edit the KERNEL_HOME variable!"
+    echo "Please edit that variable at the top of the script and run it again."
+    exit
 fi
 
 
-#################
-##  FUNCTIONS  ##
-#################
-
+###############
+#             #
+#  FUNCTIONS  #
+#             #
+###############
+#
 # Prints a formatted header; used for outlining what the script is doing to the user
 function echoText() {
-   RED="\033[01;31m"
-   RST="\033[0m"
+    RED="\033[01;31m"
+    RST="\033[0m"
 
-   echo -e ${RED}
-   echo -e "====$( for i in $( seq ${#1} ); do echo -e "=\c"; done )===="
-   echo -e "==  ${1}  =="
-   echo -e "====$( for i in $( seq ${#1} ); do echo -e "=\c"; done )===="
-   echo -e ${RST}
+    echo -e ${RED}
+    echo -e "====$( for i in $( seq ${#1} ); do echo -e "=\c"; done )===="
+    echo -e "==  ${1}  =="
+    echo -e "====$( for i in $( seq ${#1} ); do echo -e "=\c"; done )===="
+    echo -e ${RST}
 }
-
 
 # Creates a new line
 function newLine() {
-   echo -e ""
+    echo -e ""
 }
 
 
-#######################
-##  OTHER VARIABLES  ##
-#######################
+#####################
+#                   #
+#  OTHER VARIABLES  #
+#                   #
+#####################
+#
 # DO NOT EDIT
+#
+
+SOURCE_DIR=${KERNEL_HOME}/Flash-Kernel
+ANYKERNEL_DIR=${KERNEL_HOME}/Flash-AK2
+TOOLCHAIN_DIR=${KERNEL_HOME}/aarch64-linux-android-6.x
+FLASH_BRANCH=n7.1.1
 RED="\033[01;31m"
 BLINK_RED="\033[05;31m"
 RESTORE="\033[0m"
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
 KERNEL="Image.gz-dtb"
 DEFCONFIG="flash_defconfig"
-ANYKERNEL_BRANCH=angler-flash-release
+ANYKERNEL_BRANCH=angler-flash-personal-7.1.1
 ZIMAGE_DIR="${SOURCE_DIR}/arch/arm64/boot"
 DEVICE=angler
 
 
-####################
-##  SCRIPT START  ##
-####################
+##################
+#                #
+#  SCRIPT START  #
+#                #
+##################
+
+clear
+
 # Configure build
 export CROSS_COMPILE="${TOOLCHAIN_DIR}/bin/aarch64-linux-android-"
 export ARCH=arm64
 export SUBARCH=arm64
 
 
-# Clear the terminal
-clear
 
-
-# Show the version of the kernel compiling
+# Show ASCII TEXT
 echo -e ${RED}; newLine
 echo -e "======================================================================="; newLine; newLine
 echo -e "    ________    ___   _____ __  __    __ __ __________  _   __________ "
@@ -115,53 +136,79 @@ DATE_START=$(date +"%s")
 # Clean previous build and update repos
 echoText "CLEANING UP AND UPDATING"; newLine
 
-# Clean AnyKernel directory
-cd ${ANYKERNEL_DIR}
-git checkout ${ANYKERNEL_BRANCH}
-git reset --hard origin/${ANYKERNEL_BRANCH}
-git clean -f -d -x > /dev/null 2>&1
-rm -rf ${KERNEL} > /dev/null 2>&1
-git pull
+# If the head kernel directory doesn't exist, create it
+if [[ ! -d ${KERNEL_HOME} ]]; then
+    mkdir -p ${KERNEL_HOME}
+fi
 
-# Clean source directory
+# Clean AnyKernel directory if it exists, clone it if not
+if [[ -d ${ANYKERNEL_DIR} ]]; then
+    cd ${ANYKERNEL_DIR}
+    git checkout ${ANYKERNEL_BRANCH}
+    git reset --hard origin/${ANYKERNEL_BRANCH}
+    git clean -fdx > /dev/null 2>&1
+    rm -rf ${KERNEL} > /dev/null 2>&1
+    git pull
+else
+    cd ${KERNEL_HOME}
+    git clone -b ${ANYKERNEL_BRANCH} https://github.com/Flash-Kernel/AnyKernel2 Flash-AK2
+fi
+
+# Clean source directory if it exists, clone it if not
+if [[ -d ${SOURCE_DIR} ]]; then
+    cd ${SOURCE_DIR}
+    git checkout ${FLASH_BRANCH}
+    git reset --hard origin/${FLASH_BRANCH}
+    git clean -fdx > /dev/null 2>&1
+    git pull
+else
+    cd ${KERNEL_HOME}
+    git clone -b ${FLASH_BRANCH} https://github.com/Flash-ROM/kernel_huawei_angler Flash-Kernel
+fi
+
+# If the toolchain directory doesn't exist, clone it
+if [[ ! -d ${TOOLCHAIN_DIR} ]]; then
+    cd ${KERNEL_HOME}
+    git clone https://bitbucket.org/DespairFactor/aarch64-linux-android-6.x
+fi
+
+# Move into the source folder
 cd ${SOURCE_DIR}
-git checkout ${FLASH_BRANCH}
-git reset --hard origin/${FLASH_BRANCH}
-git clean -f -d -x > /dev/null 2>&1
-git pull
+
 
 # Clean make
-make clean
-make mrproper
+make clean && make mrproper
 
 
 # Set kernel version
-KERNEL_VER=$( grep -r "EXTRAVERSION = -" ${SOURCE_DIR}/Makefile | sed 's/EXTRAVERSION = -//' )
+KERNEL_VER=$( grep -r "EXTRAVERSION = -" ${SOURCE_DIR}/Makefile | sed 's/^.*F/F/' )
+# Set LOCALVERSION
+export LOCALVERSION="-$( date +%Y%m%d )"
 # Set zip name based on device and kernel version
-ZIP_NAME=${KERNEL_VER}-${DEVICE}
+ZIP_NAME=${KERNEL_VER}${LOCALVERSION}-$( date +%H%M )
 
 
 # Make the kernel
-newLine; echoText "MAKING ${KERNEL_VER}"; newLine
+newLine; echoText "MAKING ${ZIP_NAME}"; newLine
 
 make ${DEFCONFIG}
 make ${THREAD}
 
 
 # If the above was successful
-if [[ `ls ${ZIMAGE_DIR}/${KERNEL} 2>/dev/null | wc -l` != "0" ]]; then
-   BUILD_RESULT_STRING="BUILD SUCCESSFUL"
+if [[ $( ls ${ZIMAGE_DIR}/${KERNEL} 2>/dev/null | wc -l ) != "0" ]]; then
+    BUILD_RESULT_STRING="BUILD SUCCESSFUL"
 
 
-   # Make the zip file
-   newLine; echoText "MAKING FLASHABLE ZIP"; newLine
+    # Make the zip file
+    newLine; echoText "MAKING FLASHABLE ZIP"; newLine
 
-   cp -vr ${ZIMAGE_DIR}/${KERNEL} ${ANYKERNEL_DIR}/zImage
-   cd ${ANYKERNEL_DIR}
-   zip -r9 ${ZIP_NAME}.zip * -x README ${ZIP_NAME}.zip
+    cp ${ZIMAGE_DIR}/${KERNEL} ${ANYKERNEL_DIR}/zImage
+    cd ${ANYKERNEL_DIR}
+    zip -r9 ${ZIP_NAME}.zip * -x README.md ${ZIP_NAME}.zip
 
 else
-   BUILD_RESULT_STRING="BUILD FAILED"
+    BUILD_RESULT_STRING="BUILD FAILED"
 fi
 
 
@@ -170,14 +217,16 @@ cd ${HOME}
 
 
 # End the script
-newLine; echoText "${BUILD_RESULT_STRING}!"; newLine
+newLine; echoText "${BUILD_RESULT_STRING}!"
 
 DATE_END=$(date +"%s")
 DIFF=$((${DATE_END} - ${DATE_START}))
 
 echo -e ${RED}"SCRIPT DURATION: $((${DIFF} / 60)) MINUTES AND $((${DIFF} % 60)) SECONDS"
 if [[ "${BUILD_RESULT_STRING}" == "BUILD SUCCESSFUL" ]]; then
-   echo -e "ZIP LOCATION: ${ANYKERNEL_DIR}/${ZIP_NAME}.zip"
-   echo -e "SIZE: $( du -h ${ANYKERNEL_DIR}/${ZIP_NAME}.zip | awk '{print $1}' )"
+    echo -e "ZIP LOCATION: ${ANYKERNEL_DIR}/${ZIP_NAME}.zip"
+    echo -e "SIZE: $( du -h ${ANYKERNEL_DIR}/${ZIP_NAME}.zip | awk '{print $1}' )"
 fi
 echo -e ${RESTORE}
+
+unset LOCALVERSION
